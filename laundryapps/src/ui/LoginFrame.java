9583decced6f794d3f9826ae00ac2beeb1eb3aa3
L E N O V O <L E N O V O@DESKTOP-L7HR2S4>
2024-09-24 14:02:24 +0700

    PRATIKUM 2

--- laundryapps/src/ui/LoginFrame.java
+++ laundryapps/src/ui/LoginFrame.java
@@ -5,11 +5,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JComboBox;
-import java.awt.Font;
+
+import model.User;
+
 import javax.swing.JLabel;
-import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -48,45 +53,46 @@ public class LoginFrame extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnLogin = new JButton("LOGIN");
-		btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (User.login(txtUsername.getText(), txtPassword.getText())) {
-					new MainFrame().setVisible(true);
-					dispose();
-				} else {
-					JOptionPane.showMessageDialog(null,"Login Gagal");
-				
-				}
-		);
-		btnLogin.setBounds(167, 229, 89, 23);
-		contentPane.add(btnLogin);
-		
 		JLabel lblNewLabel = new JLabel("Laundry Apps");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel.setBounds(28, 0, 119, 31);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(154, 39, 153, 19);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Males nyuci,biar kami cuciin");
-		lblNewLabel_1.setBounds(28, 27, 158, 14);
+		JLabel lblNewLabel_1 = new JLabel("Anak kuliah kok nyuci, kasi ke kami aja");
+		lblNewLabel_1.setBounds(121, 61, 197, 14);
 		contentPane.add(lblNewLabel_1);
 		
+		JLabel lblNewLabel_2 = new JLabel("Username");
+		lblNewLabel_2.setBounds(78, 106, 61, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Password");
+		lblNewLabel_3.setBounds(78, 163, 78, 14);
+		contentPane.add(lblNewLabel_3);
+		
 		txtUsername = new JTextField();
-		txtUsername.setBounds(28, 88, 182, 42);
+		txtUsername.setBounds(78, 124, 253, 28);
 		contentPane.add(txtUsername);
 		txtUsername.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("USSERNAME");
-		lblNewLabel_2.setBounds(28, 63, 108, 14);
-		contentPane.add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("PASSWORD");
-		lblNewLabel_3.setBounds(28, 141, 89, 14);
-		contentPane.add(lblNewLabel_3);
-		
 		txtPassword = new JTextField();
-		txtPassword.setBounds(21, 166, 189, 42);
-		contentPane.add(txtPassword);
 		txtPassword.setColumns(10);
+		txtPassword.setBounds(78, 179, 253, 28);
+		contentPane.add(txtPassword);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(User.login(txtUsername.getText().trim(), txtPassword.getText().trim())) {
+					new MainFrame().setVisible(true);
+					dispose();
+				} else {
+					JOptionPane.showMessageDialog(null, "Login Gagal");
+				}
+			}
+		});
+		btnLogin.setBackground(new Color(128, 128, 128));
+		btnLogin.setBounds(78, 227, 253, 23);
+		contentPane.add(btnLogin);
 	}
 }
